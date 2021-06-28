fcc6478fe99ad8db86a8140bea0386b0dd053f41
Nelson Junior <47444867+Nelsonbjunior@users.noreply.github.com>
2021-06-28 05:30:54 -0400

    Add files via upload

---
+++ KOTLIN/PRATICANDO PROGRAMAÇÃO COM KOTLIN/Distância.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val distanciaMin = 2
+    val input = Scanner(System.`in`)
+
+    val distanciaKm = input.nextInt()
+    val tempo = distanciaKm * distanciaMin
+
+    println("$tempo minutos")
+}
